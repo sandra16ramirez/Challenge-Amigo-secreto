a628b6be223109e8bd8c222ef358ab1338207ac6
Sandra <sandra16ramirezp@gmail.com>
2025-03-17 14:42:30 -0500

    crea lista e implementa una funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigos = 0;
+//Lista de participantes
+let listaAmigos = [];
+
+//Captura el valor datos de entrada
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector (elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+// Función para verificar y agregar un amigo a la lista
+function verificarEntrada() {
+    let nombreAmigos = document.getElementById('amigo').value;
+  
+    if (nombreAmigos == ""){
+        alert ('Por favor, inserte un nombre.');
+        return;
+    } else {
+        listaAmigos.push(nombreAmigos);
+        console.log (listaAmigos);
+    }
+    asignarTextoElemento("#lista", listaAmigos.join(", "));
+
+}
